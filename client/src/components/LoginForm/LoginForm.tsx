383c01d33b72fcd69c654b6027057cdb8777580a
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-31 11:54:29 +0300

    feat(auth): store auth_token in localstorage and allow clearing it

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { useLoginAuthLoginPost } from '../../api/endpoints'
-import { useAuth } from '../../contexts/';
+import { useAuth, currentToken } from '../../contexts/';
 
 export function LoginForm() {
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const { setToken } = useAuth();
+    const { setToken, logout } = useAuth();
     const loginMutation = useLoginAuthLoginPost()
 
     function handleLogin(e: React.FormEvent) {
@@ -31,17 +31,28 @@ export function LoginForm() {
         console.log(`Token: ${loginMutation.data}`)
     }
 
+    if (!currentToken) {
+
+        return (
+            <form className="LoginForm" onSubmit={handleLogin}>
+                <label htmlFor="username">
+                    <span>Username</span>
+                    <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Keijo" />
+                </label>
+                <label htmlFor="password">
+                    <span>Password</span>
+                    <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        )
+    }
+
     return (
-        <form className="LoginForm" onSubmit={handleLogin}>
-            <label htmlFor="username">
-                <span>Username</span>
-                <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Keijo" />
-            </label>
-            <label htmlFor="password">
-                <span>Password</span>
-                <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            <button type="button" onClick={() => logout()}>
+                Logout
+            </button>
+        </div>
     )
 }
--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,25 +1,39 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, ReactNode, useEffect } from "react";
 
 export let currentToken: string | null = null;
 export const setCurrentToken = (t: string | null) => { currentToken = t };
 
 interface AuthContextProps {
     token: string | null;
-    setToken: (token: string | null) => void
+    setToken: (token: string | null) => void;
+    logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-    const [token, setToken] = useState<string | null>(null);
+    const [token, setTokenState] = useState<string | null>(() => localStorage.getItem("auth_token"));
 
-    const setTokenAndGlobal = (t: string | null) => {
-        setToken(t)
-        setCurrentToken(t)
+    useEffect(() => {
+        setCurrentToken(token);
+
+        if (token) {
+            localStorage.setItem("auth_token", token);
+        } else {
+            localStorage.removeItem("auth_token");
+        }
+    }, [token]);
+
+    const setToken = (t: string | null) => {
+        setTokenState(t)
+    }
+
+    const logout = () => {
+        setTokenState(null);
     }
 
     return (
-        <AuthContext.Provider value={{ token, setToken: setTokenAndGlobal }}>
+        <AuthContext.Provider value={{ token, setToken: setToken, logout }}>
             {children}
         </AuthContext.Provider>
     )
