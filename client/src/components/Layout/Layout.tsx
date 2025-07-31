1ebce8fd88c67123c78e94d350f3c8c6c2c9b42f
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-31 15:24:08 +0300

    feat(styling): use tailwind on layout

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -5,23 +5,24 @@ export function Layout() {
     const { token, logout } = useAuth();
 
     return (
-        <div className="app-layout">
-            <nav className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                {!token && <Link to="/login">Login</Link>}
+        <div className="h-screen grid grid-rows-[auto_1fr] grid-cols-[20rem_1fr] font-display">
+
+            <nav className="col-span-2 bg-zinc-600 text-white flex items-center p-4 gap-4">
+                <Link to="/" className="hover:underline">Home</Link>
+                <Link to="/users" className="hover:underline">Users</Link>
+                {!token && <Link to="/login" className="hover:underline">Login</Link>}
                 {token && (
                     <button onClick={() => logout()} >Logout</button>
                 )}
             </nav>
 
-            <aside className="sidebar">
+            <aside className="Sidebar bg-zinc-400 p-4">
                 <p>Sidebar content</p>
             </aside>
 
-            <main>
+            <main className="bg-zinc-200 p-4 overflow-y-auto">
                 <Outlet />
             </main>
-        </div>
+        </div >
     )
 }
