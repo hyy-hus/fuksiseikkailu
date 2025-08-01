7f35bd87f46e932ab421ca2c11e39ee645359800
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-01 21:18:25 +0300

    feat(routes): use admin and guest pages in routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,8 @@ import { setNavigator } from "./router/navigate";
 
 import "./App.css";
 
-const HomePage = lazy(() => import("./pages/HomePage").then((m) => ({ default: m.HomePage })));
+const GuestHomePage = lazy(() => import("./pages/GuestHomePage").then((m) => ({ default: m.GuestHomePage })));
+const AdminHomePage = lazy(() => import("./pages/AdminHomePage").then((m) => ({ default: m.AdminHomePage })));
 const UsersPage = lazy(() => import("./pages/UsersPage").then((m) => ({ default: m.UsersPage })));
 const LoginPage = lazy(() => import("./pages/LoginPage").then((m) => ({ default: m.LoginPage })));
 const PlaygroundPage = lazy(() => import("./pages/PlaygroundPage").then((m) => ({ default: m.PlaygroundPage })));
@@ -15,11 +16,14 @@ export function App() {
     return (
         <Suspense fallback={<LoadingPage />}>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<HomePage />} />
+                <Route path="/admin" element={<Layout variant="admin" />}>
+                    <Route index element={<AdminHomePage />} />
                     <Route path="users" element={<UsersPage />} />
                     <Route path="login" element={<LoginPage />} />
                     <Route path="playground" element={<PlaygroundPage />} />
+                </Route>
+                <Route path="/" element={<Layout variant="guest" />}>
+                    <Route index element={<GuestHomePage />} />
                 </Route>
                 <Route path="loading" element={<LoadingPage />} />
             </Routes>
