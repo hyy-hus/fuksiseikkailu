635f7d5f981d9d34e892f359060be6c293ca2af5
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-01 09:48:14 +0300

    feat(styling): add variants to buttons

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,7 +1,26 @@
 import { MouseEventHandler, ReactNode } from "react";
 
+import { cva } from "class-variance-authority";
+
+const button = cva(
+    "px-4 py-2 rounded font-medium disabled:opacity-50",
+    {
+        variants: {
+            variant: {
+                blue: "bg-blue-400 hover:bg-blue-500 disabled:hover:bg-blue-400 text-blue-50",
+                red: "bg-red-400 hover:bg-red-500 disabled:hover:bg-red-400 text-red-50",
+                green: "bg-green-500 hover:bg-green-600 disabled:hover:gb-green-500 text-green-50",
+            },
+        },
+        defaultVariants: {
+            variant: "blue",
+        }
+    }
+)
+
 interface ButtonProps {
     type?: "button" | "submit";
+    variant?: "blue" | "red" | "green";
     disabled?: boolean;
     children?: ReactNode;
     className?: string;
@@ -11,6 +30,7 @@ interface ButtonProps {
 
 export function Button({
     type = "button",
+    variant = "blue",
     disabled = false,
     children,
     className,
@@ -20,7 +40,7 @@ export function Button({
     return (
         <button
             name={name ?? ""}
-            className={`${className ?? ""} bg-blue-400 text-white-200 px-4 py-2 rounded hover:bg-blue-500`}
+            className={button({ variant, className })}
             type={type}
             disabled={disabled}
             onClick={onClick}
