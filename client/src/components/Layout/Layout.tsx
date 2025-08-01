2f8f321d0cc4b91ef98df9efd272ba27d26bebf1
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-01 21:17:46 +0300

    feat(layout): add admin and client specific layouts

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -6,11 +6,43 @@ import { FaSun, FaMoon } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { navigate } from "../../router/navigate";
 
-export function Layout() {
+import { cva } from "class-variance-authority";
+
+
+type LayoutVariant = "guest" | "admin";
+
+function Hr() {
+    return (
+        <hr className="text-zinc-400 dark:text-slate-600" />
+    )
+}
+
+interface LayoutProps {
+    variant: LayoutVariant;
+}
+
+export function Layout({
+    variant,
+}: LayoutProps) {
     const { token, logout } = useAuth();
 
     const [darkMode, setDarkMode] = useState<boolean>(document.documentElement.classList.contains("dark"));
     const [sidebarOpen, setSidebarOpen] = useState<boolean>(localStorage.getItem("sidebar") === "open");
+
+    const navbar = cva(
+        "col-span-2 border-b grid grid-cols-[auto_1fr_auto] items-center p-2",
+        {
+            variants: {
+                variant: {
+                    guest: "bg-zinc-300 dark:bg-slate-900 border-zinc-400 dark:border-slate-700",
+                    admin: "bg-rose-300 dark:bg-rose-950 border-rose-400 dark:border-rose-800",
+                },
+                defaultVariants: {
+                    variant: "guest",
+                }
+            }
+        }
+    )
 
     const location = useLocation();
 
@@ -48,37 +80,81 @@ export function Layout() {
     return (
         <div className={`h-screen grid grid-rows-[auto_1fr] ${sidebarOpen ? "grid-cols-[1fr_0] md:grid-cols-[auto_1fr]" : "grid-cols-[1fr]"} font-display text-zinc-900 dark:text-zinc-50`}>
 
-            <nav className="col-span-2 bg-zinc-300 dark:bg-slate-900 border-b border-zinc-400 dark:border-slate-700 grid grid-cols-[auto_1fr_auto] items-center p-2">
+            <nav className={navbar({ variant })}>
                 <Button variant="transparent" onClick={() => toggleSidebar()}><IoMenu /></Button>
-                <h1 className="font-bold hover:underline flex content-center justify-center-safe"><Link to="/">Fuksiseikkailu</Link></h1>
+                <h1 className="font-bold hover:underline flex content-center justify-center-safe">
+                    {
+                        variant === "admin" ? (
+                            <Link to="/admin">Fuksiseikkailu - Admin</Link>
+                        ) : (
+                            <Link to="/">Fuksiseikkailu</Link>
+                        )
+                    }
+                </h1>
                 <div className="inline">
                     {
-                        !token ?
-                            <Button variant="transparent" onClick={() => navigate("/login")}>Login</Button>
-                            : <Button variant="red" onClick={() => logout()} >Logout</Button>
+                        variant === "admin" ? (
+                            !token ?
+                                <Button variant="transparent" onClick={() => navigate("/login")}>Login</Button>
+                                : <Button variant="red" onClick={() => logout()} >Logout</Button>
+                        ) : (
+                            !token ?
+                                <Button variant="transparent" onClick={() => navigate("/team")}>Team</Button>
+                                : <Button variant="red" onClick={() => logout()} >Team</Button>
+                        )
                     }
                 </div>
 
             </nav>
 
-            <aside className={`Sidebar bg-zinc-300 dark:bg-slate-900 border-r border-zinc-400 dark:border-slate-700 p-4 ${sidebarOpen ? "w-full" : "overflow-hidden sr-only"}`}>
-                <ul className="grid h-full grid-rows-[repeat(3,_auto)_1fr_auto] gap-4">
-                    <li><Link to="/" className="hover:underline">Home</Link></li>
-                    <li><Link to="/users" className="hover:underline">Users</Link></li>
-                    <li><Link to="/playground" className="hover:underline">Playground</Link></li>
-                    <li></li>
-                    <li className="flex items-center justify-center">
-                        <Button onClick={toggleTheme} variant="transparent">
-                            <span>Theme:</span> {darkMode ?
-                                <FaSun /> :
-                                <FaMoon />
-                            }
-                        </Button>
-                        <Button variant="transparent">
-                            <span>Lang: EN</span>
-                        </Button>
-                    </li>
+            <aside className={`Sidebar h-full overflow-y-auto bg-zinc-300 dark:bg-slate-900 border-r border-zinc-400 dark:border-slate-700 p-4 grid grid-rows-[auto_1fr_auto] ${sidebarOpen ? "w-full" : "overflow-hidden sr-only"}`}>
+                <ul className="flex flex-col gap-4">
+                    {
+                        variant === "admin" ? (
+                            <>
+                                <li><Link to="/" className="hover:underline">Dashboard</Link></li>
+                                <li><Hr /></li>
+                                <li><Link to="/admin/teams" className="hover:underline">Teams</Link></li>
+                                <li><Link to="/admin/players" className="hover:underline">Players</Link></li>
+                                <li><Link to="/admin/checkpoints" className="hover:underline">Checkpoints</Link></li>
+                                <li><Link to="/admin/news" className="hover:underline">News</Link></li>
+                                <li><Link to="/admin/scores" className="hover:underline">Scores</Link></li>
+                                <li><Link to="/admin/photos" className="hover:underline">Photo contest</Link></li>
+                                <li><Link to="/admin/leaderboard" className="hover:underline">Leaderboard</Link></li>
+                                <li><Link to="/admin/adventures" className="hover:underline">Adventures</Link></li>
+                                <li><Hr /></li>
+                                <li><Link to="/admin/users" className="hover:underline">Users</Link></li>
+                                <li><Hr /></li>
+                                <li><Link to="/admin/playground" className="hover:underline">Playground</Link></li>
+                                <li><Link to="/admin/loading" className="hover:underline">Loading</Link></li>
+                                <li><Hr /></li>
+                                <li><Link to="/" className="hover:underline">Guest side</Link></li>
+                            </>
+
+                        ) : (
+                            <>
+                                <li><Link to="/" className="hover:underline">Home</Link></li>
+                                <li><Link to="/checkpoints" className="hover:underline">Checkpoints</Link></li>
+                                <li><Link to="/map" className="hover:underline">Map</Link></li>
+                                <li><Link to="/photos" className="hover:underline">Photo contest</Link></li>
+                                <li><Link to="/leaderboard" className="hover:underline">Leaderboard</Link></li>
+                            </>
+
+                        )
+                    }
                 </ul>
+                <div></div>
+                <div className="flex items-center justify-center mt-6">
+                    <Button onClick={toggleTheme} variant="transparent">
+                        <span>Theme:</span> {darkMode ?
+                            <FaSun /> :
+                            <FaMoon />
+                        }
+                    </Button>
+                    <Button variant="transparent">
+                        <span>Lang: EN</span>
+                    </Button>
+                </div>
             </aside>
 
             <main className={`bg-zinc-200 dark:bg-slate-900 dark:text-zinc-100 p-4 overflow-y-auto ${sidebarOpen ? "hidden md:block md:w-full" : ""}`}>
