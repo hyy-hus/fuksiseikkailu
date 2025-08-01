48da2442c3417142c9f6840d31d5a425adc62e2d
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-01 12:56:33 +0300

    feat(ui): use icons on buttons

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,19 +1,26 @@
 import { Outlet, Link } from "react-router-dom";
 import { useAuth } from "@auth";
 import { Button } from "@components/Button";
+import { IoMenu } from "react-icons/io5";
+import { FaSun, FaMoon } from "react-icons/fa";
+import { useState } from "react";
 
 export function Layout() {
     const { token, logout } = useAuth();
+
+    const [darkMode, setDarkMode] = useState<boolean>(document.documentElement.classList.contains("dark"));
 
     function toggleTheme() {
         const html = document.documentElement;
         const isDark = html.classList.contains("dark");
         if (isDark) {
             html.classList.remove("dark");
+            setDarkMode(false);
         }
         else {
             html.classList.add("dark");
             localStorage.setItem("theme", "dark");
+            setDarkMode(true);
         }
     }
 
@@ -21,7 +28,7 @@ export function Layout() {
         <div className="h-screen grid grid-rows-[auto_1fr] grid-cols-[20rem_1fr] font-display text-zinc-900 dark:text-zinc-50">
 
             <nav className="col-span-2 bg-zinc-300 dark:bg-slate-900 border-b border-zinc-400 dark:border-slate-700 flex items-center p-4 gap-4">
-                <Button>Sidebar</Button>
+                <Button variant="transparent"><IoMenu /></Button>
                 <Link to="/" className="hover:underline">Home</Link>
                 <Link to="/users" className="hover:underline">Users</Link>
                 <Link to="/playground" className="hover:underline">Playground</Link>
@@ -34,7 +41,12 @@ export function Layout() {
 
             <aside className="Sidebar bg-zinc-300 dark:bg-slate-900 border-r border-zinc-400 dark:border-slate-700 p-4">
                 <p>Sidebar content</p>
-                <Button onClick={toggleTheme}>Toggle theme</Button>
+                <Button onClick={toggleTheme} variant="transparent">
+                    <span>Theme:</span> {darkMode ?
+                        <FaSun /> :
+                        <FaMoon />
+                    }
+                </Button>
             </aside>
 
             <main className="bg-zinc-200 dark:bg-slate-900 dark:text-zinc-100 p-4 overflow-y-auto">
