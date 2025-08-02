0a24ed2eab6e8324f20a748b5f92e86af2eadaae
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-02 16:36:54 +0300

    feat(components): create form-components

---
+++ client/src/components/Forms/ModifyAdventure.tsx
@@ -0,0 +1,55 @@
+import { getListAdventuresAdventuresGetQueryKey, useFetchAdventureAdventuresAdventureIdGet, useUpdateAdventureAdventuresAdventureIdPatch } from "@api/endpoints";
+import { Form, FieldDef } from "./Form";
+import { ModifyAdventure } from "@api/model";
+import { t } from "i18next";
+import { useQueryClient } from "@tanstack/react-query";
+
+interface AdventureFormProps {
+    adventureId: number;
+}
+
+export function ModifyAdventureForm({ adventureId }: AdventureFormProps) {
+    const queryClient = useQueryClient();
+    const { data, isLoading, isError, error } = useFetchAdventureAdventuresAdventureIdGet(adventureId);
+    const updateMutation = useUpdateAdventureAdventuresAdventureIdPatch();
+
+    if (isLoading) {
+        return (
+            <div>{t("loading")}</div>
+        );
+    }
+
+    if (isError || updateMutation.isError) {
+        return (
+            <div className="text-red-600">
+                {t("network-error")}: {String((error as any || updateMutation.error)?.message ?? error)}
+            </div>
+        )
+    }
+
+    if (!data) {
+        return <div>{t("not-found")}</div>
+    }
+
+
+    function handleSave(item: ModifyAdventure) {
+        updateMutation.mutateAsync(
+            { adventureId: adventureId, data: item }
+        ).then(() => {
+            queryClient.invalidateQueries({
+                queryKey: getListAdventuresAdventuresGetQueryKey(),
+            })
+        })
+    }
+
+    const fields: FieldDef<ModifyAdventure>[] = [
+        { key: "name", name: t("name"), type: "text" },
+        { key: "year", name: t("year"), type: "number" },
+        { key: "ongoing", name: t("year"), type: "toggle" },
+        { key: "test", name: t("test"), type: "toggle" },
+    ]
+
+    return (
+        <Form item={data.data as ModifyAdventure} fields={fields} onSave={handleSave} />
+    )
+}
