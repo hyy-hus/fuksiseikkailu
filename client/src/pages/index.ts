af667b39511c49e8157921d7cb67a5010cbb6ff2
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-02 23:08:02 +0300

    feat(pages): add TeamsPage

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,6 +2,7 @@ export { GuestHomePage } from "./GuestHomePage";
 export { AdminHomePage } from "./AdminHomePage";
 export { UsersPage } from "./UsersPage";
 export { LoginPage } from "./LoginPage";
+export { TeamsPage } from "./TeamsPage";
 export { PlaygroundPage } from "./PlaygroundPage";
 export { CheckpointsPage } from "./CheckpointsPage";
 export { NotFound } from "./NotFound";
