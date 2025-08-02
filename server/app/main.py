138cf79273eee69067b1de15364b7d673b9d0ded
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-02 20:17:01 +0300

    feat(api): include checkpoint routes in app

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import users, auth, adventures
+from app.routers import users, auth, adventures, checkpoints
 
 app = FastAPI(root_path="/api")
 
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(adventures.router)
+app.include_router(checkpoints.router)
