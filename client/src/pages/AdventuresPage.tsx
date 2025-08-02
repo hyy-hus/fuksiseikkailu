a6c172f823f959e4bc996ebe900c6ff026b24527
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-02 10:46:22 +0300

    feat(pages): handle selections in AdventurePage

--- client/src/pages/AdventuresPage.tsx
+++ client/src/pages/AdventuresPage.tsx
@@ -1,14 +1,32 @@
+import { PublicAdventure } from "@api/model";
 import { AdventureList } from "@components/Lists";
+import { useState } from "react";
 import { useTranslation } from "react-i18next"
 
 export function AdventuresPage() {
-
     const { t } = useTranslation();
+
+    const [selected, setSelected] = useState<PublicAdventure[]>([]);
 
     return (
         <div className="flex flex-col gap-4">
             <h2>{t("adventures")}</h2>
-            <AdventureList />
+            <AdventureList onChange={setSelected} handleEdit={(item: PublicAdventure) => setSelected([item])} />
+            {
+                selected.length === 0 && (
+                    <span>None selected</span>
+                )
+            }
+            {
+                selected.length === 1 && (
+                    <span>One selected: {selected[0].name}</span>
+                )
+            }
+            {
+                selected.length > 1 && (
+                    <span>Multiple selected</span>
+                )
+            }
         </div>
     )
 }
