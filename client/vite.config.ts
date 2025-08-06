c4a706068f51f75243778728e77fec4c6ec006d0
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-06 15:54:55 +0300

    feat(container): add support for dev profile

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -49,10 +49,10 @@ export default defineConfig({
     ],
     server: {
         host: '0.0.0.0',
-        port: 5173,
+        port: 3000,
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "http://web-dev:8000",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
             }
