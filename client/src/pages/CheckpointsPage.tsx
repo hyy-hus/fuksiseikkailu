1ca8414073a74bc71b91ab9077ffb26e39d4d969
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-08 10:06:45 +0300

    feat(components): split into separate components

--- client/src/pages/CheckpointsPage.tsx
+++ client/src/pages/CheckpointsPage.tsx
@@ -5,7 +5,7 @@ import { CheckpointList } from "@components/Lists";
 import { useAdventure } from "@contexts/AdventureContext";
 import { useQueryClient } from "@tanstack/react-query";
 import { t } from "i18next";
-import { useCallback, useEffect, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next"
 
 import { clsx } from "clsx";
@@ -43,6 +43,180 @@ interface ColumnValue {
     outputColumn: string;
     identifier: boolean;
 }
+
+interface ColumnMappingRowProps {
+    key: string;
+    header: string;
+    value?: ColumnValue;
+    onChange: (val: string) => void;
+    columnValues: ColumnValue[];
+    setColumnValues: React.Dispatch<React.SetStateAction<ColumnValue[]>>;
+}
+
+function ColumnMappingRow({ key, header, value, columnValues, setColumnValues }: ColumnMappingRowProps) {
+    return (
+        <li key={key}
+            className="contents"
+        >
+            <div className="flex items-center"><span>{header}</span></div>
+            <div className="flex items-center justify-center"><FaArrowRight /></div>
+            <div className="flex items-center w-full">
+                <Select options={adminCheckpointOptions}
+                    value={value?.outputColumn ?? "skip"}
+                    onChange={(e) => {
+                        setColumnValues((prev) => {
+                            return prev.map(val => {
+                                if (val.inputColumn === header) {
+                                    return { ...val, outputColumn: e.target.value };
+                                }
+
+                                return val;
+                            })
+                        })
+                    }} />
+            </div>
+            <div className="flex items-center justify-center w-full">
+                <Toggle
+                    value={(() => {
+                        const newVal = columnValues.find(val => val.inputColumn === header)?.identifier
+                        return newVal
+                    })()}
+                    onChange={(newValue) => {
+                        setColumnValues((prev) => {
+                            return prev.map(val => {
+                                if (val.inputColumn === header) {
+                                    return { ...val, identifier: newValue };
+                                }
+
+                                return val;
+                            })
+                        })
+                    }}
+                />
+            </div>
+        </li>
+    );
+}
+
+interface ColumnMappingProps {
+    headers: string[];
+    columnValues: ColumnValue[];
+    setColumnValues: React.Dispatch<React.SetStateAction<ColumnValue[]>>
+}
+
+function ColumnMapping({ headers, columnValues, setColumnValues }: ColumnMappingProps) {
+
+    function updateColumnValue(
+        header: string,
+        newPartial: Partial<ColumnValue>
+    ) {
+        setColumnValues(prev =>
+            prev.map(val =>
+                val.inputColumn === header ? { ...val, ...newPartial } : val
+            )
+        );
+    }
+
+    return (
+        <ul className="w-full grid grid-cols-[minmax(auto,30rem)_auto_auto_auto] gap-4 items-center">
+            <div className="contents">
+                <span>{t("import-data-column")}</span>
+                <span></span>
+                <span className="">{t("save-value-as")}</span>
+                <span className="flex justify-center">{t("identifier")}</span>
+            </div>
+            {
+                headers.map(header => (
+                    <ColumnMappingRow
+                        key={header}
+                        header={header}
+                        value={columnValues.find(val => val.inputColumn === header)}
+                        onChange={(newOutputKey) => updateColumnValue(header, { outputColumn: newOutputKey })}
+                        columnValues={columnValues}
+                        setColumnValues={setColumnValues}
+                    />
+                ))
+            }
+        </ul>
+    )
+}
+
+interface UploadTableProps {
+    data: string[][];
+    setData: React.Dispatch<React.SetStateAction<string[][]>>
+}
+
+function UploadTable({ data, setData }: UploadTableProps) {
+    return (
+        <div className="overflow-x-auto max-w-full border border-slate-700">
+            <table className="w-fit table-auto border-collapse">
+                {data.map((row, i) => (
+                    <tr key={i} className={clsx(
+                        i === 0 ? "bg-slate-700 text-white font-bold sticky top-0 z-10" : "odd:bg-slate-800"
+                    )}>
+                        {
+                            row.map((cell, j) => (
+                                <td key={j}
+                                    contentEditable={i !== 0}
+                                    suppressContentEditableWarning
+                                    onBlur={(e) => {
+                                        if (i === 0) {
+                                            return;
+                                        }
+
+                                        const newValue = e.currentTarget.textContent ?? "";
+                                        setData(prev => {
+                                            const newData = [...prev];
+                                            if (!newData[i]) {
+                                                return prev;
+                                            }
+
+                                            newData[i] = [...newData[i]];
+                                            newData[i][j] = newValue;
+
+                                            return newData;
+                                        });
+                                    }}
+                                    className={clsx(
+                                        "p-2 text-sm whitespace-nowrap max-w-100 overflow-x-hidden text-ellipsis",
+                                        i > 0 && "border-t border-slate-700",
+                                        j > 0 && "border-l border-slate-700"
+                                    )}
+                                >
+                                    {cell}
+                                </td>
+                            ))
+                        }
+                    </tr>
+                ))
+                }
+            </table>
+        </div>
+    );
+}
+
+interface PasteAreaProps {
+    value: string;
+    onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+function PasteArea({ value, onChange }: PasteAreaProps) {
+    return (
+        <div>
+            <label className="w-full">
+                You can insert data copied from excel or google sheets here. You should probably sanitize the input by doing find and replaces for <pre>'\n' -&gt; ''</pre> and <pre>'\t' -&gt; ''</pre> first.
+                <textarea rows={5}
+                    className="w-full rounded border border-zinc-400 bg-zinc-300 dark:border-slate-700 dark:bg-slate-800 p-4"
+                    onChange={onChange}
+                    value={value}
+                >
+                </textarea>
+            </label>
+        </div>
+    );
+}
+
+
 
 function BulkImport() {
     const [data, setData] = useState<string>("");
@@ -99,110 +273,13 @@ function BulkImport() {
     return (
         <form className="flex flex-col gap-4">
             <h3>{t("bulk-import")}</h3>
-            <label className="w-full">
-                You can insert data copied from excel or google sheets here. You should probably sanitize the input by doing find and replaces for <pre>'\n' -&gt; ''</pre> and <pre>'\t' -&gt; ''</pre> first.
-                <textarea rows={5}
-                    className="w-full rounded border border-zinc-400 bg-zinc-300 dark:border-slate-700 dark:bg-slate-800 p-4"
-                    onChange={(e) => setData(e.target.value)}>
-                    {data}
-                </textarea>
-            </label>
-            <div className="overflow-x-auto max-w-full border border-slate-700">
-                <table className="w-fit table-auto border-collapse">
-                    {uploadData.map((row, i) => (
-                        <tr key={i} className={clsx(
-                            i === 0 ? "bg-slate-700 text-white font-bold sticky top-0 z-10" : "odd:bg-slate-800"
-                        )}>
-                            {
-                                row.map((cell, j) => (
-                                    <td key={j}
-                                        contentEditable={i !== 0}
-                                        suppressContentEditableWarning
-                                        onBlur={(e) => {
-                                            if (i === 0) {
-                                                return;
-                                            }
-
-                                            const newValue = e.currentTarget.textContent ?? "";
-                                            setUploadData(prev => {
-                                                const newData = [...prev];
-                                                if (!newData[i]) {
-                                                    return prev;
-                                                }
-
-                                                newData[i] = [...newData[i]];
-                                                newData[i][j] = newValue;
-
-                                                return newData;
-                                            });
-                                        }}
-                                        className={clsx(
-                                            "p-2 text-sm whitespace-nowrap max-w-100 overflow-x-hidden text-ellipsis",
-                                            i > 0 && "border-t border-slate-700",
-                                            j > 0 && "border-l border-slate-700"
-                                        )}
-                                    >
-                                        {cell}
-                                    </td>
-                                ))
-                            }
-                        </tr>
-                    ))
-                    }
-                </table>
-            </div>
-            <ul className="w-full grid grid-cols-[minmax(auto,30rem)_auto_auto_auto] gap-4 items-center">
-                <div className="contents">
-                    <span>{t("import-data-column")}</span>
-                    <span></span>
-                    <span className="">{t("save-value-as")}</span>
-                    <span className="flex justify-center">{t("identifier")}</span>
-                </div>
-                {
-                    headers.map(header => (
-                        <li key={header}
-                            className="contents"
-                        >
-                            <div className="flex items-center"><span>{header}</span></div>
-                            <div className="flex items-center justify-center"><FaArrowRight /></div>
-                            <div className="flex items-center w-full">
-                                <Select options={adminCheckpointOptions}
-                                    value={columnValues.find(val => val.inputColumn === header)?.outputColumn}
-                                    onChange={(e) => {
-                                        setColumnValues((prev) => {
-                                            return prev.map(val => {
-                                                if (val.inputColumn === header) {
-                                                    return { ...val, outputColumn: e.target.value };
-                                                }
-
-                                                return val;
-                                            })
-                                        })
-                                    }} />
-                            </div>
-                            <div className="flex items-center justify-center w-full">
-                                <Toggle
-                                    value={(() => {
-                                        const newVal = columnValues.find(val => val.inputColumn === header)?.identifier
-                                        return newVal
-                                    })()}
-                                    onChange={(newValue) => {
-                                        setColumnValues((prev) => {
-                                            return prev.map(val => {
-                                                if (val.inputColumn === header) {
-                                                    return { ...val, identifier: newValue };
-                                                }
-
-                                                return val;
-                                            })
-                                        })
-                                    }}
-                                />
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
+            <PasteArea value={data} onChange={(e) => setData(e.target.value)} />
+            <UploadTable data={uploadData} setData={setUploadData} />
+            <ColumnMapping
+                headers={headers}
+                columnValues={columnValues}
+                setColumnValues={setColumnValues}
+            />
             <Button variant="green" onClick={uploadItems}
             >
                 Upload
