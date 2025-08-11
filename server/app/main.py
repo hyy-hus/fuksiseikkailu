559839fb9da0073047b89490989d1526dcc1dd9d
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-08-11 16:18:07 +0300

    feat(backend): include scores.router in the api

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import users, auth, adventures, checkpoints, teams, push, news
+from app.routers import users, auth, adventures, checkpoints, teams, push, news, scores
 
 app = FastAPI(root_path="/api")
 
@@ -11,3 +11,4 @@ app.include_router(checkpoints.router)
 app.include_router(teams.router)
 app.include_router(push.router)
 app.include_router(news.router)
+app.include_router(scores.router)
